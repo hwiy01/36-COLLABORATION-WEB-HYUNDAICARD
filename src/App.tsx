ceb63446c1cd2df0f67af89c8391b6cd2e029c64
kojesung <js95112345@gmail.com>
2025-05-11 02:29:51 +0900

    init:path alias 세팅 및 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { TestIcon } from "./assets/svg";
+import { testValue } from "@/shared/assets/test";
+import { ohNO } from '@/shared/assets/asd';
 
 function App() {
+  const a = testValue;
+  const ab = ohNO;
+  console.log(a)
   return <><TestIcon width={900} height={900}></TestIcon></>;
 }
 
---
+++ src/shared/assets/asd.ts
@@ -0,0 +1,1 @@
+export const ohNO = 123123;
