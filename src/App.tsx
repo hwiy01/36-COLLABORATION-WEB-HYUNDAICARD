703731e4c109cd0773ede03d6a6c9b39fd391e19
hwiy01 <vdocom02@naver.com>
2025-05-11 22:33:19 +0900

    init: Tanstack-query 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from "./shared/utils/query-client";
+
 function App() {
-  return <></>
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+   </>
+  )
 }
 
 export default App;
