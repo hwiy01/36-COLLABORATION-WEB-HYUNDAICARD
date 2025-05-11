428f489e3ee75706648b18d20dc125dbf63fa13c
kojesung <js95112345@gmail.com>
2025-05-11 22:15:41 +0900

    chore:테스트용 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { TestIcon } from "./assets/svg";
-import { testValue } from "@/shared/assets/test";
-import { ohNO } from '@/shared/assets/asd';
-
 function App() {
-  const a = testValue;
-  const ab = ohNO;
-  console.log(a)
-  return <><TestIcon width={900} height={900}></TestIcon></>;
+  return <></>
 }
 
 export default App;
