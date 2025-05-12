1e31016c64776c2090d1673ef3efa846a27d44ab
gwagjiug <kwjo0228@naver.com>
2025-05-13 01:24:56 +0900

    feat: 라우터를 포함한 App 컴포넌트 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./shared/utils/query-client";
+import Router from "@/shared/router/router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-   </>
-  )
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Router />
+				</BrowserRouter>
+				<div style={{ fontSize: "16px" }}>
+					<ReactQueryDevtools initialIsOpen={false} />
+				</div>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
