f6be423e93112bcf3e9780518a28c5f47fbdc5ec
kojesung <js95112345@gmail.com>
2025-05-14 03:13:54 +0900

    feat: 공통 컴포넌트 사용 주석 추가

--- src/shared/components/card/card.tsx
+++ src/shared/components/card/card.tsx
@@ -4,11 +4,17 @@ import type { ReactNode } from "react";
 type CardMode = "vertical" | "horizontal"; // vertical : 세로 모드, horizontal : 가로 모드
 
 interface CardProps {
-	mode?: CardMode;
+	mode?: CardMode; // "vertical" | "horizontal"
 	cardIcon: ReactNode;
 	cardName?: string; // 카드 이름
 }
 
+/**
+ * 
+ * @param mode "vertical(세로)" | "horizontal(가로)" 중 하나 입력 default = "vertical"
+ * @param cardIcon 렌더링 시킬 요소
+ * @param cardName 카드 이름
+ */
 const Card = ({ mode = "vertical", cardIcon, cardName = "" }: CardProps) => {
 	return (
 		<section className={cardContainer[mode]}>
