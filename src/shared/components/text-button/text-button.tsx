7a1e0ce486a2286079b9159d105b44ac3fd9dcce
kojesung <js95112345@gmail.com>
2025-05-14 04:24:51 +0900

    feat: 주석 추가

--- src/shared/components/text-button/text-button.tsx
+++ src/shared/components/text-button/text-button.tsx
@@ -3,10 +3,16 @@ import { baseTextButtonContainer, buttonText } from "./text-button.css";
 import { RightArrow } from "src/assets/svg";
 
 interface TextButtonProps {
-	text?: string;
-	img?: ReactNode;
+	text?: string; // 버튼 내부에 작성할 문구
+	img?: ReactNode; // gap : 4 뒤에 렌더링할 요소(ex. 이미지)
 }
 
+/**
+ *
+ * @param text 버튼 내부에 작성할 문구
+ * @param img gap : 4 뒤에 렌더링할 요소(ex. 이미지)
+ * @returns
+ */
 const TextButton = ({
 	text = "전체보기",
 	img = <RightArrow width={5} height={5} />,
