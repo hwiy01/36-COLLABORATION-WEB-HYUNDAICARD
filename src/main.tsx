1df6c3fc417d40ec3812d23cf7bae1009034ba02
kojesung <js95112345@gmail.com>
2025-05-14 03:14:55 +0900

    delete: 테스트용 코드 및 svgr 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import Card from './shared/components/card/card.tsx'
-import { CardH, CardV } from './assets/svg/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Card cardIcon={<CardV/>} mode='vertical' cardName='the Red'/>
-    <Card cardIcon={<CardH/>}mode='horizontal' cardName='American Express
-The Platinum Card  Edition2'/>
   </StrictMode>,
 )
