5fd64bc9f9128cee88bbbabe27321c6d56dc5e1d
kojesung <js95112345@gmail.com>
2025-05-14 13:28:19 +0900

    feat: onClick props 전달 추가

--- src/shared/components/text-button/text-button.tsx
+++ src/shared/components/text-button/text-button.tsx
@@ -1,16 +1,18 @@
-import type { ReactNode } from "react";
+import type { MouseEvent, ReactNode } from "react";
 import { baseTextButtonContainer, buttonText } from "./text-button.css";
 import { RightArrow } from "src/assets/svg";
 
 interface TextButtonProps {
 	text?: string; // 버튼 내부에 작성할 문구
 	img?: ReactNode; // gap : 4 뒤에 렌더링할 요소(ex. 이미지)
+	onClick?: (event: MouseEvent<HTMLButtonElement>) => void; // 마우스 click시 이벤트
 }
 
 /**
  *
  * @param text 버튼 내부에 작성할 문구
  * @param img gap : 4 뒤에 렌더링할 요소(ex. 이미지)
+ * @param onClick void 반환 형식, 마우스 클릭했을 때 이벤트
  * @returns
  */
 const TextButton = ({
