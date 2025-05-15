abaea37a528d44d852f1948b486b1f8273acc921
kojesung <js95112345@gmail.com>
2025-05-16 03:45:48 +0900

    chore: 공통 컴포넌트 주석 추가

--- src/shared/components/chips/chip.tsx
+++ src/shared/components/chips/chip.tsx
@@ -10,29 +10,37 @@ import {
   otherModeStyle,
 } from "./chip.css";
 
-export type mode = "input" | "filter" | "selected" | "dropdown";
+export type mode = "input" | "filter" | "selected" | "dropdown"; // 유틸함수에 타입 전달용
 
 interface ChipProps {
-  mode: "input" | "filter" | "selected" | "dropdown";
-  content: string;
+  mode: "input" | "filter" | "selected" | "dropdown"; // 4가지 모드 중 하나
+  content: string; // chip에 들어갈 내용
   handleClickCloseBtn?: () => void;
-  onClick?: () => void;
-  dropdownFlag: boolean;
+  handleClickDropdown?: () => void;
+  dropdownFlag?: boolean;
   className?: string;
 }
-
+/**
+ *
+ * @param mode "input" | "filter" | "selected" | "dropdown" 중 하나의 모드
+ * @param content chip에 들어갈 내용, 내용의 길이 기반하여 width 자동 조절
+ * @param handleClickCloseBtn mode = "input"일 때 생기는 x 버튼 click에 부여할 함수
+ * @param handleClickDropdown mode = "dropdown"일 때 dropdown 클릭했을 때 부여할 함수
+ * @param dropdownFlag mode = "dropdown"일 때 화살표 모양 control
+ * @returns
+ */
 const Chip = ({
   mode = "dropdown",
   content,
   handleClickCloseBtn,
   dropdownFlag,
-  onClick,
+  handleClickDropdown,
 }: ChipProps) => {
   const size = getSizeByContent(content, mode);
 
   if (mode === "dropdown") {
     return (
-      <button className={baseFrame} onClick={onClick} type="button">
+      <button className={baseFrame} onClick={handleClickDropdown} type="button">
         <section className={innerFrame}>
           <p>{content}</p>
           {dropdownFlag ? (
