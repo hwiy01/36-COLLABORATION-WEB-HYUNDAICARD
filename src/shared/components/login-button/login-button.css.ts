bf2c337e8c2cba86059d793bbbd892ff3073a164
jjangminii <wjdals030328@catholic.ac.kr>
2025-05-16 04:49:05 +0900

    recipes 의존성 설치

--- src/shared/components/login-button/login-button.css.ts
+++ src/shared/components/login-button/login-button.css.ts
@@ -1,68 +1,79 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
 import { themeVars } from "src/styles";
 import { color } from "src/styles/tokens/color.css";
 
-export const buttonBase = style({
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  border: "none",
-  cursor: "pointer",
-  textAlign: "center",
-  boxSizing: "border-box",
-  ...themeVars.fontStyles.title_bold_13,
-});
+export const button = recipe({
+  base: {
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    boxSizing: "border-box",
+    border: "none",
+    cursor: "pointer",
 
-export const colorVariants = styleVariants({
-  whiteGray: {
-    backgroundColor: color.gray100,
-    color: color.h_black,
-    selectors: {
-      "&:hover": {
-        backgroundColor: color.gray200,
-        cursor: "pointer",
-      },
-    },
+    // ✅ 폰트 통합
+    ...themeVars.fontStyles.title_bold_13,
   },
-  blackGray: {
-    backgroundColor: color.h_black,
-    color: color.h_white,
-    selectors: {
-      "&:hover": {
-        backgroundColor: color.gray800,
-        cursor: "pointer",
-      },
-    },
-  },
-  whiteOutline: {
-    backgroundColor: color.h_white,
-    color: color.h_black,
-    borderColor: color.h_black,
-    border: "2px solid",
-    selectors: {
-      "&:hover": {
-        borderColor: color.gray200,
-        backgroundColor: color.gray200,
-        cursor: "pointer",
-      },
-    },
-  },
-});
 
-export const sizeVariants = styleVariants({
-  small: {
-    width: "7.3rem",
-    height: "3.2rem",
-    borderRadius: "0.8rem",
+  variants: {
+    color: {
+      whiteGray: {
+        backgroundColor: color.gray100,
+        color: color.h_black,
+        selectors: {
+          "&:hover": {
+            backgroundColor: color.gray200,
+            cursor: "pointer",
+          },
+        },
+      },
+      blackGray: {
+        backgroundColor: color.h_black,
+        color: color.h_white,
+        selectors: {
+          "&:hover": {
+            backgroundColor: color.gray800,
+            cursor: "pointer",
+          },
+        },
+      },
+      whiteOutline: {
+        backgroundColor: color.h_white,
+        color: color.h_black,
+        border: "2px solid",
+        borderColor: color.h_black,
+        selectors: {
+          "&:hover": {
+            backgroundColor: color.gray200,
+            borderColor: color.gray200,
+            cursor: "pointer",
+          },
+        },
+      },
+    },
+
+    size: {
+      small: {
+        width: "7.3rem",
+        height: "3.2rem",
+        borderRadius: "0.8rem",
+      },
+      medium: {
+        width: "10.9rem",
+        height: "3.8rem",
+        borderRadius: "0.4rem",
+      },
+      large: {
+        width: "22.1rem",
+        height: "4.5rem",
+        borderRadius: "0.8rem",
+      },
+    },
   },
-  medium: {
-    width: "10.9rem",
-    height: "3.8rem",
-    borderRadius: "0.4rem",
-  },
-  large: {
-    width: "22.1rem",
-    height: "4.5rem",
-    borderRadius: "0.8rem",
+
+  defaultVariants: {
+    color: "blackGray",
+    size: "medium",
   },
 });
