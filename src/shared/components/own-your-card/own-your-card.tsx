b3953ef81c05bff08c709ad929e26d05af954ff1
kojesung <js95112345@gmail.com>
2025-05-17 06:44:03 +0900

    feat: 버튼 타입 명시

--- src/shared/components/own-your-card/own-your-card.tsx
+++ src/shared/components/own-your-card/own-your-card.tsx
@@ -1,32 +1,36 @@
 import { HcLogo } from "src/assets/svg";
 import {
-	cardContainer,
-	cardEntry,
-	cardHoverEffect,
-	cardOverlay,
-	flagComment,
-	logoStyle,
+  cardContainer,
+  cardEntry,
+  cardHoverEffect,
+  cardOverlay,
+  flagComment,
+  logoStyle,
 } from "./own-your-card.css";
 
 interface OwnYourCardProps {
-	onClick: () => void;
-	flag: boolean;
+  onClick: () => void;
+  flag: boolean;
 }
 
 const OwnYourCard = ({ onClick, flag = false }: OwnYourCardProps) => {
-	return (
-		<section className={cardContainer}>
-			<button onClick={onClick} className={`${cardEntry} ${cardHoverEffect}`}>
-				<div className={cardOverlay}></div>
-				<HcLogo
-					className={logoStyle}
-					width={175.66017150878906}
-					height={34.51904296875}
-				/>
-				{flag && <span className={flagComment}>내게 맞는 카드 확인하기</span>}
-			</button>
-		</section>
-	);
+  return (
+    <section className={cardContainer}>
+      <button
+        type="button"
+        onClick={onClick}
+        className={`${cardEntry} ${cardHoverEffect}`}
+      >
+        <div className={cardOverlay}></div>
+        <HcLogo
+          className={logoStyle}
+          width={175.66017150878906}
+          height={34.51904296875}
+        />
+        {flag && <span className={flagComment}>내게 맞는 카드 확인하기</span>}
+      </button>
+    </section>
+  );
 };
 
 export default OwnYourCard;
