b71544250347497522079196619e5ae5abe428e1
kojesung <js95112345@gmail.com>
2025-05-17 07:06:24 +0900

    fix: px -> rem, type 내용 직접 지정

--- src/shared/components/card/card.css.ts
+++ src/shared/components/card/card.css.ts
@@ -3,47 +3,46 @@ import { fontStyles } from "src/styles/tokens/font-styles.css";
 
 // card section 기본 스타일
 const baseCardConstainer = style({
-	display: "flex",
-	flexDirection: "column",
-	gap: "10px",
-	boxSizing: "border-box", // 피그마 기준 개발하기 위해 패딩과 테부리 width/height에 포함
-	transition: "padding 150ms linear",
-	alignItems: "center",
-	textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  boxSizing: "border-box", // 피그마 기준 개발하기 위해 패딩과 테부리 width/height에 포함
+  transition: "padding 150ms linear",
+  alignItems: "center",
+  textAlign: "center",
 });
 
 // 가로-세로 모드에 따라 다르게 변화시킬 스타일
 export const cardContainer = styleVariants({
-	vertical: [
-		baseCardConstainer,
-		{
-			width: "124px",
-			height: "163px",
-			padding: "10px 23px 0px 23px",
-		},
-	],
-	horizontal: [
-		baseCardConstainer,
-		{
-			width: "150px",
-			height: "151px",
-			padding: "10px 0px 0px 0px",
-		},
-	],
+  vertical: [
+    baseCardConstainer,
+    {
+      width: "12.4rem",
+      height: "16.3rem",
+      padding: "1rem 2.3rem 0rem 2.3rem",
+    },
+  ],
+  horizontal: [
+    baseCardConstainer,
+    {
+      width: "15rem",
+      height: "15.1rem",
+      padding: "1rem 0rem 0rem 0rem",
+    },
+  ],
 });
 
-
-// 호버 시에는 공통적으로 위로 10px
+// 호버 시에는 공통적으로 위로 1rem
 globalStyle(
-	`${cardContainer.vertical}:hover, ${cardContainer.horizontal}:hover`,
-	{
-		paddingTop: "0px",
-		paddingBottom: "10px",
-	},
+  `${cardContainer.vertical}:hover, ${cardContainer.horizontal}:hover`,
+  {
+    paddingTop: "0rem",
+    paddingBottom: "1rem",
+  },
 );
 
 // 정의된 폰트에 fontSize만 덮어서 추가
-export const cardNameSpan = styleVariants({
-	vertical: [{ ...fontStyles.caption_bold, fontSize: "13px" }],
-	horizontal: [{ ...fontStyles.caption_bold, fontSize: "12px" }],
+export const cardNameP = styleVariants({
+  vertical: [{ ...fontStyles.caption_bold, fontSize: "1.3rem" }],
+  horizontal: [{ ...fontStyles.caption_bold, fontSize: "1.2rem" }],
 });
--- src/shared/components/card/card.tsx
+++ src/shared/components/card/card.tsx
@@ -1,27 +1,25 @@
-import { cardContainer, cardNameSpan } from "./card.css";
 import type { ReactNode } from "react";
-
-type CardMode = "vertical" | "horizontal"; // vertical : 세로 모드, horizontal : 가로 모드
+import { cardContainer, cardNameP } from "./card.css";
 
 interface CardProps {
-	mode?: CardMode; // "vertical" | "horizontal"
-	cardIcon: ReactNode;
-	cardName?: string; // 카드 이름
+  mode?: "vertical" | "horizontal"; // "vertical" | "horizontal"
+  cardIcon: ReactNode;
+  cardName?: string; // 카드 이름
 }
 
 /**
- * 
+ *
  * @param mode "vertical(세로)" | "horizontal(가로)" 중 하나 입력 default = "vertical"
  * @param cardIcon 렌더링 시킬 요소
  * @param cardName 카드 이름
  */
 const Card = ({ mode = "vertical", cardIcon, cardName = "" }: CardProps) => {
-	return (
-		<section className={cardContainer[mode]}>
-			{cardIcon}
-			<span className={cardNameSpan[mode]}>{cardName}</span>
-		</section>
-	);
+  return (
+    <section className={cardContainer[mode]}>
+      {cardIcon}
+      <p className={cardNameP[mode]}>{cardName}</p>
+    </section>
+  );
 };
 
 export default Card;
