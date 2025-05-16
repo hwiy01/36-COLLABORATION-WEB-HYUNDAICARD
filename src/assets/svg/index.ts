a3793046bbf7b00209b535bc85fd5889fc5b1081
kojesung <js95112345@gmail.com>
2025-05-17 07:09:29 +0900

    delete: 불필요한 코드 삭제

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,6 +1,3 @@
 export { default as IconArrowDown } from "./IconArrowDown";
 export { default as IconArrowUp } from "./IconArrowUp";
 export { default as IconClose } from "./IconClose";
-
-export { default as CardH } from "./CardH";
-export { default as CardV } from "./CardV";
