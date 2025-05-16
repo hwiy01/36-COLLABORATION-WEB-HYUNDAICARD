a1d549883aa6b00e8be97f38b8dcd5e60df362dd
kojesung <js95112345@gmail.com>
2025-05-17 06:43:29 +0900

    chore: 주석 조절 및 css 단위 수정

--- src/shared/components/own-your-card/own-your-card.css.ts
+++ src/shared/components/own-your-card/own-your-card.css.ts
@@ -3,99 +3,96 @@ import { color } from "src/styles/tokens/color.css";
 import { fontStyles } from "../../../styles/tokens/font-styles.css";
 
 const floatingAnimation = keyframes({
-	"0%": {
-		paddingTop: "10px",
-		paddingBottom: "0px",
-	},
-	"50%": {
-		paddingTop: "0px",
-		paddingBottom: "10px",
-	},
-	"100%": {
-		paddingTop: "10px",
-		paddingBottom: "0px",
-	},
+  "0%": {
+    paddingTop: "1rem",
+    paddingBottom: "0rem",
+  },
+  "50%": {
+    paddingTop: "0rem",
+    paddingBottom: "1rem",
+  },
+  "100%": {
+    paddingTop: "1rem",
+    paddingBottom: "0rem",
+  },
 });
 
-// 컴포넌트 전체 css(플로팅 영역까지 포함)
+// 컴포넌트 전체 css(플로팅 영역까지 포함 - padding으로 조절)
 export const cardContainer = style({
-	width: "248px",
-	height: "392px",
-	paddingTop: "10px", // 플로팅(?)할 패딩 공간
-	paddingBottom: "0px",
-	animation: `${floatingAnimation} 2s ease-in-out infinite`,
+  width: "24.8rem",
+  height: "39.2rem",
+  paddingTop: "1rem",
+  paddingBottom: "0rem",
+  animation: `${floatingAnimation} 2s ease-in-out infinite`,
 
-	":hover": {
-		// 호버 시 패딩값 조절하면서 플로팅
-		animation: "none",
-		paddingTop: "0px",
-		paddingBottom: "10px",
-	},
+  ":hover": {
+    animation: "none",
+    paddingTop: "0rem",
+    paddingBottom: "1rem",
+  },
 
-	transition: "padding-top 1s ease, padding-bottom 1s ease",
+  transition: "padding-top 1s ease, padding-bottom 1s ease",
 });
 
-// 플로팅 영역 제외 카드 부분 css
+// 플로팅 영역 제외 카드 부분 css - position 활용한 overlay
 export const cardEntry = style({
-	display: "flex",
-	alignItems: "center",
-	flexDirection: "column",
-	background: color.h_primary_gradient_1,
-	width: "100%",
-	height: "100%",
-	borderRadius: "15px",
-	gap: "40.48px",
-	position: "relative", // 오버레이를 위한
-	border: "none",
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  background: color.h_primary_gradient_1,
+  width: "100%",
+  height: "100%",
+  borderRadius: "1.5rem",
+  gap: "4.048rem",
+  position: "relative",
+  border: "none",
 
-	":active": {
-		// 클릭 시 배경 색 진하게
-		background: color.h_primary_gradient_3,
-	},
+  // 클릭 시
+  ":active": {
+    background: color.h_primary_gradient_3,
+  },
 });
 
 // 오버레이 될 투명한 커버
 export const cardOverlay = style({
-	position: "absolute",
-	top: 0,
-	left: 0,
-	height: "100%",
-	width: "100%",
-	borderRadius: "15px",
-	backgroundColor: "rgba(255,255,255,0.2)",
-	opacity: 0,
-	pointerEvents: "none",
-	zIndex: 50,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  height: "100%",
+  width: "100%",
+  borderRadius: "1.5rem",
+  backgroundColor: "rgba(255,255,255,0.2)",
+  opacity: 0,
+  pointerEvents: "none",
+  zIndex: 50,
 });
 
 export const cardHoverEffect = style({
-	// 호버 시 오버레이 on
-	":hover": {
-		[`& .${cardOverlay}`]: {
-			opacity: 1,
-		},
-	},
-	// 클릭하면 오버레이 off
-	":active": {
-		[`& .${cardOverlay}`]: {
-			opacity: 0,
-		},
-	},
+  ":hover": {
+    [`& .${cardOverlay}`]: {
+      opacity: 1,
+    },
+  },
+  ":active": {
+    [`& .${cardOverlay}`]: {
+      opacity: 0,
+    },
+  },
 });
 
 export const logoStyle = style({
-	marginTop: "122px",
+  marginTop: "12.2rem",
 });
 
 export const flagComment = style({
-	width: "221px",
-	height: "45px",
-	borderRadius: "8px",
-	backgroundColor: "black",
-	color: "white",
-	display: "flex",
-	justifyContent: "center",
-	alignItems: "center",
-	...fontStyles.hyundai_title30,
-	fontSize: "13px",
+  width: "22.1rem",
+  height: "4.5rem",
+  borderRadius: "0.8rem",
+  backgroundColor: "black",
+  color: "white",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  ...fontStyles.title_bold_13,
+  fontWeight: "400",
 });
