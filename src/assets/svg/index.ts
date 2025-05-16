8a828711d33d98878d8b9e63afc09ec895dfe894
kojesung <js95112345@gmail.com>
2025-05-17 00:33:54 +0900

    fix: 불필요한 이미지 삭제 및 global css 적용 위치 변경

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,4 +2,3 @@ export { default as ImgEvent1 } from "./ImgEvent1";
 export { default as IconArrowDown } from "./IconArrowDown";
 export { default as IconArrowUp } from "./IconArrowUp";
 export { default as IconClose } from "./IconClose";
-export { default as SelectedS } from "./SelectedS";
