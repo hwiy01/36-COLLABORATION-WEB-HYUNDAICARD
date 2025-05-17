382eaa0dc7ead0a3d1dd5eeddc0a91b2d4c96dfe
jjangminii <wjdals030328@catholic.ac.kr>
2025-05-18 04:51:31 +0900

    콘솔, 주석제거

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,7 +1,8 @@
 export { default as IconArrowDown } from "./IconArrowDown";
 export { default as IconArrowUp } from "./IconArrowUp";
 export { default as IconClose } from "./IconClose";
-export { default as RightArrow } from "./RightArrow";
 export { default as SvgHyunddaiSymbol } from "./SvgHyunddaiSymbol";
+
+export { default as RightArrow } from "./RightArrow";
 export { default as AdPlay } from "./AdPlay";
 export { default as AdStop } from "./AdStop";
