158cec96cd3f1480b13656e0058757310fc8f7c2
jjangminii <wjdals030328@catholic.ac.kr>
2025-05-18 23:37:24 +0900

    refactor: 타입 변경

--- src/shared/components/main-event/main-event.tsx
+++ src/shared/components/main-event/main-event.tsx
@@ -1,20 +1,15 @@
-import type { ReactNode } from "react";
 import * as styles from "./main-event.css";
 
 interface MainEventProps {
-  cardIcon: ReactNode;
-  textContent: string; // 한 줄로 받는 텍스트
+  cardIconUrl: string;
+  textContent: string;
 }
 
-const MainEvent = ({ cardIcon, textContent }: MainEventProps) => {
+const MainEvent = ({ cardIconUrl, textContent }: MainEventProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          src={cardIcon as string}
-          alt="Card Icon"
-        />
+        <img className={styles.image} src={cardIconUrl} alt="Card Icon" />
       </div>
       <div className={styles.textContainer}>
         <span className={styles.text}>{textContent}</span>
