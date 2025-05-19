63d4966d96a32a2bc318e48d7b774cacdd2dbee6
gwagjiug <kwjo0228@naver.com>
2025-05-20 01:07:02 +0900

    feat: client config 파일 수정

--- src/shared/apis/config.ts
+++ src/shared/apis/config.ts
@@ -2,7 +2,7 @@ import type { paths } from "@/shared/types/api";
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
 
-const client = createFetchClient<paths>({
+export const client = createFetchClient<paths>({
   baseUrl: import.meta.env.VITE_API_BASE_URL,
 });
 
