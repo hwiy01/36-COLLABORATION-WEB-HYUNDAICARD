5787de881601465f4518698165b0e71fc04a96a4
gwagjiug <kwjo0228@naver.com>
2025-05-19 22:27:01 +0900

    feat: axios 설정 수정

--- src/shared/apis/axios-instance.ts
+++ src/shared/apis/axios-instance.ts
@@ -4,15 +4,14 @@ const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    Accept: "*/*",
   },
-  timeout: 10000, // 몇으로 설정할까요오..
 });
 
 apiRequest.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response.data.data;
+  },
   (error) => {
-    console.error("API Error:", error);
     return Promise.reject(error);
   },
 );
