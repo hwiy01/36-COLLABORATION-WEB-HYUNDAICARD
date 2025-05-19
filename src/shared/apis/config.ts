59a79af5e54fde0bf6d1209238d92d8bf63b36a1
gwagjiug <kwjo0228@naver.com>
2025-05-20 00:48:20 +0900

    feat: openapi 세팅

--- src/shared/apis/config.ts
+++ src/shared/apis/config.ts
@@ -1,1 +1,9 @@
 import type { paths } from "@/shared/types/api";
+import createFetchClient from "openapi-fetch";
+import createClient from "openapi-react-query";
+
+const client = createFetchClient<paths>({
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
+});
+
+export const $api = createClient(client); // 이 부분만 추가
