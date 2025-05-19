33b1da5ac2cf845dd07513ffcf1b55127b27d1f9
kojesung <js95112345@gmail.com>
2025-05-19 16:07:32 +0900

    feat: 응답 인터셉터 설정

--- src/shared/apis/axios-instance.ts
+++ src/shared/apis/axios-instance.ts
@@ -9,4 +9,12 @@ const apiRequest = axios.create({
   timeout: 10000, // 몇으로 설정할까요오..
 });
 
+apiRequest.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  },
+);
+
 export default apiRequest;
