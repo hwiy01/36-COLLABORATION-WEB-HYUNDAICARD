379d26c752fca888fc89e122257db929bf3c8858
hwiy01 <vdocom02@naver.com>
2025-05-19 22:40:40 +0900

    feat: 화살표 버튼 누르면 자연스러운 애니메이션 되도록

--- src/shared/components/carousel/carousel.tsx
+++ src/shared/components/carousel/carousel.tsx
@@ -4,7 +4,7 @@ import { IconLeftArrow, IconRightArrow } from "src/assets/svg";
 import { CarouselImageContainer } from "./carousel-image-container";
 import { useDrag } from "./use-drag.ts";
 
-const AUTO_SLIDE_INTERVAL = 1000;
+const AUTO_SLIDE_INTERVAL = 5000;
 
 export interface ImageItem {
   id: number;
@@ -31,9 +31,16 @@ export const Carousel = ({ width, height, imgList }: CarouselProps) => {
   // transition 없는 상태에서, fake 요소인 인덱스 0으로 이동
   // 원래 transition이 500ms에 걸쳐서 일어났지만,
   // 감지하기 힘들정도로 짧은 시간(10ms) 만에 transition 없이 clone 요소로 순간이동 -> 무한 스크롤처럼 보임
-  const resetIndexAndTransition = () => {
+  const resetIndexToFirstAndTransition = () => {
     setTimeout(() => {
       setCarouselIndex(0);
+      setCarouselTransition('none');
+    }, 5);
+  };
+
+  const resetIndexToLastAndTransition = () => {
+    setTimeout(() => {
+      setCarouselIndex(displayList.length - 1);
       setCarouselTransition('none');
     }, 5);
   };
@@ -43,8 +50,9 @@ export const Carousel = ({ width, height, imgList }: CarouselProps) => {
   useEffect(() => {
     if (!pause) {
       const timer = setInterval(() => {
+        // 마지막 인덱스 도달 시
         if (carouselIndex === displayList.length - 2) {
-          resetIndexAndTransition();
+          resetIndexToFirstAndTransition();
         }
         setCarouselIndex((prev) => (prev + 1) % (displayList.length - 1));
         setCarouselTransition('transform 0.5s ease-in-out');
@@ -66,19 +74,22 @@ export const Carousel = ({ width, height, imgList }: CarouselProps) => {
   };
 
   const onMoveNext = () => {
-    if(carouselIndex === displayList.length - 1) {
-      resetIndexAndTransition();
+    if(carouselIndex === displayList.length - 2) {
+      // 맨 마지막 페이지인 경우
+      resetIndexToFirstAndTransition();
     } else {
-      setCarouselIndex((prev) => (prev + 1) % displayList.length);
       setCarouselTransition('transform 0.5s ease-in-out');
+      setCarouselIndex((prev) => (prev + 1) % displayList.length);
     }
   }
 
   const onMovePrev = () => {
-    if(carouselIndex === displayList.length - 1) {
-      resetIndexAndTransition();
+    if(carouselIndex === 1) {
+      // 맨 첫 페이지인 경우
+      resetIndexToLastAndTransition();
     } else {
-      setCarouselIndex(prev => prev - 1);
+      setCarouselTransition('transform 0.5s ease-in-out');
+      setCarouselIndex((prev) => (prev + 1) % displayList.length);
     }
   }
 
@@ -87,7 +98,7 @@ export const Carousel = ({ width, height, imgList }: CarouselProps) => {
       className={styles.container}
       style={{ width: `${width}rem`, height: `${height}rem` }}
     >
-      <button type="button" onClick={() => setCarouselIndex((prev) => Math.max(prev - 1, 0))} className={styles.leftArrow} style={{ zIndex: 5 }}>
+      <button type="button" onClick={()=>onMovePrev()} className={styles.leftArrow} style={{ zIndex: 5 }}>
         <IconLeftArrow style={{ color: "white", width: "70px" }} />
       </button>
 
@@ -106,7 +117,7 @@ export const Carousel = ({ width, height, imgList }: CarouselProps) => {
         ))}
       </div>
 
-      <button type="button" onClick={() => setCarouselIndex((prev) => prev + 1)} className={styles.rightArrow} style={{ zIndex: 2 }}>
+      <button type="button" onClick={() => onMoveNext()} className={styles.rightArrow} style={{ zIndex: 2 }}>
         <IconRightArrow style={{ color: "white", height: "70px" }} />
       </button>
     </div>
