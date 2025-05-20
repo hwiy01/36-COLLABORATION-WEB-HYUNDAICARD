375d2c02fcf1e54da2b1d009fb8c404428bc3510
hwiy01 <vdocom02@naver.com>
2025-05-21 00:38:12 +0900

    fix: 불필요 테스트 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import { queryClient } from "./shared/utils/query-client";
 import ThemeProvider from "./styles/theme-provider";
-import { Dropdown } from "./shared/components/dropdown/dropdown";
-import { useState } from "react";
-import { mockDropdownList } from "./shared/mocks/mock-dropdown-list";
 
 function App() {
-  const [temp, setTemp] = useState(mockDropdownList[0]);
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <ThemeProvider>
             <Router />
-            <Dropdown content={'쇼핑/소비'} setSelectedItem={setTemp} dropdownList={mockDropdownList}/>
           </ThemeProvider>
         </BrowserRouter>
         <div style={{ fontSize: "16px" }}>
