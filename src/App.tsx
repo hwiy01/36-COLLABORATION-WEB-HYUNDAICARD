cd744e52444382594381cec4eeffa5c5a6ab58b6
gwagjiug <kwjo0228@naver.com>
2025-05-21 15:50:29 +0900

    refactor: App 컴포넌트에서 캐러셀 및 프로그래스바 관련 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import { queryClient } from "./shared/utils/query-client";
 import ThemeProvider from "./styles/theme-provider";
-import { Carousel } from "./shared/components/carousel/carousel";
-import { mockImgList } from "./shared/mocks/mockImgList";
-import ProgressBar from "./shared/components/progress-bar/progress-bar";
-import { useCarouselProgressSync } from "./shared/components/carousel/use-carousel-progress-sync";
 
 function App() {
   return (
