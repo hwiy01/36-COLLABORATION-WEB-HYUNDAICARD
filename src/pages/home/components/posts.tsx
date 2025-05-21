f3e3aa6ed312f452d96e860ac81b904fa7a043cb
gwagjiug <kwjo0228@naver.com>
2025-05-22 03:20:07 +0900

    feat: posts 컴포넌트에서 useGetPosts 훅 제거 및 useGetQuery 훅 추가

--- src/pages/home/components/posts.tsx
+++ src/pages/home/components/posts.tsx
@@ -1,20 +1,14 @@
 import MainEvent from "@/shared/components/main-event/main-event";
-import * as styls from "./post.css";
-import { useGetPosts } from "../hooks/get-posts";
+import * as styles from "./post.css";
 
 const Posts = () => {
-  const { data } = useGetPosts();
-
   return (
-    <div className={styls.postContainer}>
-      {data.map((post, idx) => (
-        <MainEvent
-          key={idx}
-          cardIconUrl={post?.imageUrl}
-          textContent={post?.title}
-          cardName={post?.title}
-        />
-      ))}
+    <div className={styles.postContainer}>
+      {/* <MainEvent
+        cardIconUrl={post?.imageUrl}
+        textContent={post?.title}
+        cardName={post?.title}
+      /> */}
     </div>
   );
 };
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,15 @@
+import { useGetQuery } from "@/shared/hooks/hooks";
 import Posts from "./components/posts";
 import * as styles from "./home.css";
+import { END_POINTS } from "@/shared/apis/end-point";
 
 const Home = () => {
+  const postsData = useGetQuery(END_POINTS.posts, {
+    method: "GET",
+  });
+
+  console.log("postsData", postsData.data);
+
   return (
     <section className={styles.eventCardContainer}>
       <Posts />
