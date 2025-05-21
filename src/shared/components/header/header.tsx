a5656d2f9ace6e856e5b694ad07bd666bc00139a
gwagjiug <kwjo0228@naver.com>
2025-05-21 12:45:24 +0900

    feat: 헤더 hover 동작구현

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IconApplepay,
   IconHamburge,
@@ -7,36 +7,62 @@ import {
   VectorStroke,
 } from "src/assets/svg";
 import * as styles from "./header.css";
+import SubContent from "./sub-content";
 
 const MAIN_NAV_ITEMS = ["Account", "카드", "혜택", "금융", "컬처", "고객 지원"];
 const UTILITY_NAV_ITEMS = ["법인", "가맹점", "소비자보호 포털", "상품공지실"];
 
 const Header = () => {
+  const [isCardHovered, setIsCardHovered] = useState(false);
+  const handleCardHover = (isHovering: boolean) => {
+    setIsCardHovered(isHovering);
+  };
+
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerLeftContainer}>
-        <SvgHyundaiLogo width={"15.5rem"} height={"4.3rem"} />
-        <div className={styles.leftItemContainer}>
-          {MAIN_NAV_ITEMS.map((item) => (
-            <a className={styles.mainItems} key={item}>
-              {item}
-            </a>
-          ))}
-          <VectorStroke width={"0.1rem"} height={"1.8rem"} />
-          <IconApplepay width={"5rem"} height={"2.1rem"} />
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerLeftContainer}>
+          <SvgHyundaiLogo width={"15.5rem"} height={"4.3rem"} />
+          <div className={styles.leftItemContainer}>
+            {MAIN_NAV_ITEMS.map((item) => (
+              <a
+                className={styles.mainItems}
+                key={item}
+                onMouseEnter={
+                  item === "카드" ? () => handleCardHover(true) : undefined
+                }
+                onMouseLeave={
+                  item === "카드" ? () => handleCardHover(false) : undefined
+                }
+              >
+                {item}
+              </a>
+            ))}
+            <VectorStroke width={"0.1rem"} height={"1.8rem"} />
+            <IconApplepay width={"5rem"} height={"2.1rem"} />
+          </div>
         </div>
-      </div>
-      <div className={styles.headerRightContainer}>
-        {UTILITY_NAV_ITEMS.map((item, index) => (
-          <React.Fragment key={item}>
-            <p>{item}</p>
-            {index === 1 && <VectorStroke width="0.1rem" height="1.8rem" />}
-          </React.Fragment>
-        ))}
-        <IconSearch width={"2.8rem"} height={"2.8rem"} />
-        <IconHamburge width={"2.8rem"} height={"2.8rem"} />
-      </div>
-    </header>
+        <div className={styles.headerRightContainer}>
+          {UTILITY_NAV_ITEMS.map((item, index) => (
+            <React.Fragment key={item}>
+              <p>{item}</p>
+              {index === 1 && <VectorStroke width="0.1rem" height="1.8rem" />}
+            </React.Fragment>
+          ))}
+          <IconSearch width={"2.8rem"} height={"2.8rem"} />
+          <IconHamburge width={"2.8rem"} height={"2.8rem"} />
+        </div>
+      </header>
+
+      {isCardHovered && (
+        <div
+          onMouseEnter={() => handleCardHover(true)}
+          onMouseLeave={() => handleCardHover(false)}
+        >
+          <SubContent />
+        </div>
+      )}
+    </>
   );
 };
 
