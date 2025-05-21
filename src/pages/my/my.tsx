8d74b9b4d815c1e632d77d1ac23ab0c2fb68f0af
kojesung <js95112345@gmail.com>
2025-05-22 00:26:31 +0900

    feat: 태그 호출 및 tag 타입 response 고려하여 수정

--- src/pages/my/my.tsx
+++ src/pages/my/my.tsx
@@ -1,11 +1,18 @@
+import {
+  type TagCategory,
+  initialTagCategories,
+} from "@/shared/mocks/mock-tags";
+import { useState } from "react";
 import Accordion from "./components/accordion/accordion";
 
 const My = () => {
+  const [tags, setTags] = useState<TagCategory[]>(initialTagCategories);
+  // api 연동할 때 response -> Tag type으로 convert 해줘야함!
   return (
     <div style={{ width: "26.6rem" }}>
-      <Accordion />
-      <Accordion />
-      <Accordion />
+      {tags.map((item) => (
+        <Accordion key={item.category} title={item.category} />
+      ))}
     </div>
   );
 };
