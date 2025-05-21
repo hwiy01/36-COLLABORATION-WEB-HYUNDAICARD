0609a8eff089b0b55a1d1a7795b9d3b04b50b866
gwagjiug <kwjo0228@naver.com>
2025-05-22 03:04:40 +0900

    feat: axios 클라이언트를 openapi-fetch로 교체

--- src/shared/apis/config.ts
+++ src/shared/apis/config.ts
@@ -1,25 +1,6 @@
-import axios from "axios";
+import createClient from "openapi-fetch";
+import type { paths } from "../types/api";
 
-export const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-});
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export function get<T>(...args: Parameters<typeof instance.get>) {
-  return instance.get<T>(...args);
-}
-
-export function post<T>(...args: Parameters<typeof instance.post>) {
-  return instance.post<T>(...args);
-}
-
-export function put<T>(...args: Parameters<typeof instance.put>) {
-  return instance.put<T>(...args);
-}
-
-export function patch<T>(...args: Parameters<typeof instance.patch>) {
-  return instance.patch<T>(...args);
-}
-
-export function del<T>(...args: Parameters<typeof instance.delete>) {
-  return instance.delete<T>(...args);
-}
+export const client = createClient<paths>({ baseUrl });
