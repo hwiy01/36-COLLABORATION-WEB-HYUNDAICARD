f11896e7355cf47510dab250f5a3869d0f04239b
gwagjiug <kwjo0228@naver.com>
2025-05-21 15:34:18 +0900

    feat: API 엔드포인트 상수 이름 변경 및 타입 제한 함수 추가

--- src/shared/apis/end-point.ts
+++ src/shared/apis/end-point.ts
@@ -1,8 +1,12 @@
 import type { paths } from "@/shared/types/api";
 
+/**
+ * paths에 정의된 API endpoint만 입력 가능하도록 제한하는 함수
+ * 오타나 잘못된 경로 입력을 방지할 수 있음
+ */
 const createEndpoint = <T extends keyof paths>(endpoint: T): T => endpoint;
 
-export const API_ENDPOINTS = {
+export const END_POINTS = {
   tags: createEndpoint("/api/v1/tags"),
   cards: createEndpoint("/api/v1/cards"),
   search: createEndpoint("/api/v1/cards/search"),
