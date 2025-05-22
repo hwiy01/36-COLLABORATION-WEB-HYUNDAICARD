463e96690cde8d03a3cda4a3fdbe9e6d3664b913
kojesung <js95112345@gmail.com>
2025-05-23 00:04:39 +0900

    feat: rangeSlider 기능 완성(마우스 추적 후 상태 변경)

--- src/pages/my/components/range-slider/range-slider.tsx
+++ src/pages/my/components/range-slider/range-slider.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   baseSlider,
   handle,
@@ -21,6 +21,8 @@ const RangeSlider = ({
 }: RangeSliderProps) => {
   const [minIndex, setMinIndex] = useState(initMinIdx); // 하한값
   const [maxIndex, setMaxIndex] = useState(initMaxIdx); // 상한값
+  const [isDragging, setIsDragging] = useState<"min" | "max" | null>(null); // 두 개 중 어떤 핸들 드래그 중인지
+
   const clintMousePointer = useRef<HTMLDivElement>(null);
 
   /**
@@ -30,11 +32,14 @@ const RangeSlider = ({
    */
   const getPercentage = (index: number) => (index / (range.length - 1)) * 100;
 
+  /**
+   *
+   * @param clientMouseX 마우스의 X위치
+   * (마우스의 left - 요소의 left) / 요소의 width
+   * 계산해서 어느 정도 비율에 있는지 계산 -> index 반환
+   * @returns 반올림된 index
+   */
   const getIdxByMousePoint = (clientMouseX: number) => {
-    /**
-     * (마우스의 left - 요소의 left) / 요소의 width
-     * 계산해서 어느 정도 비율에 있는지 계산 -> index 반환
-     */
     if (!clintMousePointer.current) {
       return 0;
     }
@@ -45,19 +50,48 @@ const RangeSlider = ({
 
     return index;
   };
-
+  /**
+   * 마우스가 어디로 움직이는지 값 받아서(index로) 해당 값 setIndex 해주기
+   */
   const handleMouseMove = (e: MouseEvent) => {
-    /**
-     * 마우스가 어디로 움직이는지 값 받아서(index로) 해당 값 setIndex 해주기
-     */
+    const newIdx = getIdxByMousePoint(e.clientX);
+
+    if (isDragging === "min") {
+      // 하한 값 핸들하고 있을 때
+      if (0 <= newIdx && newIdx <= maxIndex) {
+        // 상한 값보다 작거나 같을 때만 min값 세팅
+        setMinIndex(newIdx);
+      }
+    } else if (isDragging === "max") {
+      // 동일하게
+      if (newIdx >= minIndex && newIdx < range.length) {
+        setMaxIndex(newIdx);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isDragging) {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", () => setIsDragging(null));
+
+      return () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", () => setIsDragging(null));
+      };
+    }
+  }, [isDragging]);
+
+  const handleMouseDown = (type: "min" | "max") => (e: React.MouseEvent) => {
+    setIsDragging(type);
+    e.preventDefault();
   };
 
   return (
     <div className={rangeSlideContainer}>
-      <section className={baseSlider}>
+      <section className={baseSlider} ref={clintMousePointer}>
         {/* 선택된 구간(검은색) */}
         <div
-          ref={clintMousePointer}
           className={selectedTrack}
           style={{
             left: `${getPercentage(minIndex)}%`,
@@ -68,10 +102,12 @@ const RangeSlider = ({
         <div
           className={handle}
           style={{ left: `${getPercentage(minIndex)}%` }}
+          onMouseDown={handleMouseDown("min")}
         />
         <div
           className={handle}
           style={{ left: `${getPercentage(maxIndex)}%` }}
+          onMouseDown={handleMouseDown("max")}
         />
       </section>
       <section className={labelSection}>
