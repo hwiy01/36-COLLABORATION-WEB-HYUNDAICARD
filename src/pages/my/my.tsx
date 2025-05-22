489a2de0b0e0e8f84605e14bd1d2eca4c81126b0
gwagjiug <kwjo0228@naver.com>
2025-05-22 16:47:37 +0900

    feat: Header 컴포넌트에 location prop 추가 및 SubHeader 조건부 렌더링 구현

--- src/pages/my/my.tsx
+++ src/pages/my/my.tsx
@@ -1,10 +1,7 @@
-import { SubHeader } from "./components/card-recommendation/sub-header/sub-header";
-
 const My = () => {
   return (
-    <>  
-      <SubHeader />  
-      <div>하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이</div>
+    <>
+      <div></div>
     </>
   );
 };
