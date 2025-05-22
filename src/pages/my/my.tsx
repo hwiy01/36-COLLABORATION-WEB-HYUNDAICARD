bd7c62fb441fbf480c8a1d6685e4a72507f817f6
kojesung <js95112345@gmail.com>
2025-05-23 03:28:15 +0900

    feat: lottie 추가

--- src/pages/my/my.tsx
+++ src/pages/my/my.tsx
@@ -1,19 +1,23 @@
 import Accordion from "./components/accordion/accordion";
+import SpinCardLottie from "./components/spin-card-lottie";
 import { useTagFilter } from "./hooks/tag-filter";
 
 const My = () => {
   const { tags, toggleTag } = useTagFilter();
 
   return (
-    <div style={{ width: "26.6rem" }}>
-      {tags.map((item, categoryIdx) => (
-        <Accordion
-          key={item.category}
-          title={item.category ?? ""}
-          tags={item.tags ?? []}
-          onTagClick={(tagId: string) => toggleTag(categoryIdx, tagId)}
-        />
-      ))}
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "26.6rem" }}>
+        {tags.map((item, categoryIdx) => (
+          <Accordion
+            key={item.category}
+            title={item.category ?? ""}
+            tags={item.tags ?? []}
+            onTagClick={(tagId: string) => toggleTag(categoryIdx, tagId)}
+          />
+        ))}
+      </div>
+      <SpinCardLottie></SpinCardLottie>
     </div>
   );
 };
